763fb5c13d91236b853398eec1ce82635fd35dd3
glodie <glodie.tshimini@gmail.com>
2024-09-11 14:49:52 +0200

    feat: directives and options, composition APIs

---
+++ 0-exercices/corrections/ex4/src/components/CompositionAPI.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="hello">
+    <input type="text" placeholder="Saisir quelque chose" v-model.trim="phrase">
+    <p :class="{red : phrase.length < 10 }">{{ phrase.length < 10 ? 'Trop court': 'OK'  }}</p>
+    <ul>
+      <li>{{ ageStr }}</li>
+      <li>{{ fullname }}</li>
+    </ul>
+    <button @click="hello">Hello</button>
+  </div>
+</template>
+
+<script setup>
+import { computed, defineProps } from 'vue'
+// Composition API
+// props = données exterieurs transmis par un composant parent
+const props = defineProps({
+  msg: {
+    type: String,
+    required: false,
+    default: 'valeur par défaut'
+  },
+  title:  {
+    type: String,
+    required: true
+  }
+})
+// données internes
+const user = {
+  firstname: "Glodie",
+  lastname: "Tshimini",
+  age: 32,
+}
+const phrase =  ''
+
+// Données calculées
+
+const ageStr = computed(() => `${user.age} ans`)
+const fullname = computed(() => `${user.firstname} ${user.lastname}`)
+
+// Fonctions
+
+const hello = (e) => {
+  console.log('event', e)
+  alert('hello')
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+.red {
+  color: red;
+  font-weight: bold;
+}
+</style>
---
+++ 0-exercices/corrections/ex4/src/components/OptionsAPI.vue
@@ -0,0 +1,101 @@
+<template>
+  <div class="hello">
+    <input type="text" placeholder="Saisir quelque chose" v-model.trim="phrase">
+    <p :class="{red : phrase.length < 10 }">{{ phrase.length < 10 ? 'Trop court': 'OK'  }}</p>
+    <ul v-if="false">
+      <li>{{ ageStr }}</li>
+      <li>{{ fullname }}</li>
+      {{ user }}
+    </ul>
+    <hr>
+    <h2>Liste des taches</h2>
+    <ul>
+      <li v-for="(item, index) in tasks" :key="item">{{ index }}{{ item }}</li>
+    </ul>
+    <table>
+      <tbody>
+        <tr>
+          <td
+            v-for="item in completedTasks"
+            :key="item.name"
+          >
+          <span>{{ item.name }}</span>
+          <input type="checkbox" :checked="item.isDone"></td>
+        </tr>
+      </tbody>
+    </table>
+    <button @click="hello">Hello</button>
+  </div>
+</template>
+
+<script>
+// Options API
+export default {
+  name: "HelloWorld",
+  // props = données exterieurs transmis par un composant parent
+  props: {
+    msg: String,
+  },
+  // données internes
+  data() {
+    return {
+      user: {
+        firstname: "Glodie",
+        lastname: "Tshimini",
+        age: 32,
+      },
+      phrase: '',
+      tasks: ['manger', 'dormir', 'coder'],
+      completedTasks : [
+        {
+          name: 'courir',
+          isDone: false
+        },
+        {
+          name: 'lire',
+          isDone: true
+        },
+        {
+          name: 'marcher',
+          isDone: true
+        }
+      ]
+    };
+  },
+  computed: { // données calculées ou transformées à partir des données internes
+    ageStr() {
+      return `j'ai ${this.user.age} ans`; // Concaténation  que "j'ai " + this.age +  "ans"
+    },
+    fullname() {
+      return `${this.user.firstname} ${this.user.lastname}`
+    }
+  },
+  methods: {
+    hello() {
+      alert('hello')
+    }
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+.red {
+  color: red;
+  font-weight: bold;
+}
+</style>
