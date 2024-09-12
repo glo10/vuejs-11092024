2d7bb860d70e8770bd2cb0a5a8030efa9a5b013f
glodie <glodie.tshimini@gmail.com>
2024-09-12 10:12:16 +0200

    feat: starting testing HelloWorld Component

---
+++ 0-exercices/corrections/ex5/src/components/HelloWorld.vue
@@ -0,0 +1,35 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="greetings">
+    <h1>Liste des courses</h1>
+    <input type="text" placeholder="Saisir le nom de l'article">
+  </div>
+</template>
+
+<style scoped>
+h1 {
+  font-weight: 500;
+  font-size: 2.6rem;
+  position: relative;
+  top: -10px;
+}
+
+h3 {
+  font-size: 1.2rem;
+}
+
+.greetings h1,
+.greetings h3 {
+  text-align: center;
+}
+
+@media (min-width: 1024px) {
+  .greetings h1,
+  .greetings h3 {
+    text-align: left;
+  }
+}
+</style>
