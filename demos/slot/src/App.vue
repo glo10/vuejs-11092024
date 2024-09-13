1c9530b304aab0e04868acf2ae2713eb0ef1961b
glodie <glodie.tshimini@gmail.com>
2024-09-13 10:42:19 +0200

    feat: demo slot

---
+++ demos/slot/src/App.vue
@@ -0,0 +1,65 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import WelcomeItem from './components/WelcomeItem.vue'
+import IconFacebook from '@/components/icons/IconFacebook.vue'
+import BaseLayout from '@/components/BaseLayout.vue'
+</script>
+
+<template>
+<BaseLayout>
+  <template #header>
+    <IconFacebook/>
+  </template>
+  <template #content>
+    <WelcomeItem>
+      <template #heading>
+        <ul>
+          <li>Item 1</li>
+          <li>Item 2</li>
+        </ul>
+      </template>
+    </WelcomeItem>
+    <WelcomeItem>
+      <template #heading>
+        <table>
+          <tbody>
+          <tr>
+            <td>Item 1</td>
+            <td>Item 2</td>
+          </tr>
+          </tbody>
+        </table>
+      </template>
+    </WelcomeItem>
+  </template>
+</BaseLayout>
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
+</style>
---
+++ demos/slot/src/components/BaseLayout.vue
@@ -0,0 +1,7 @@
+<template>
+    <div>
+        <slot name="header">Contenu par défaut header</slot>
+        <slot name="content"></slot>
+        <slot name="footer">Copyright 2042</slot>
+    </div>
+</template>
